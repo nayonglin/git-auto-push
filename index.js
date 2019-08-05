8d723254caffb9e6a8806cd17e7677dac44b6690
nayonglin <nayonglin@huya.com>
2019-08-05 17:39:07 +0800

    提交

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ try {
         }, function (error, stdout, stderr) {
 
             if (error) {
-                console.error(`你现在所在分支：exec error: ${error}`);
+                console.error(`工作树修改：exec error: ${error}`);
                 return;
             }
 
-            console.log('你现在所在分支：' + stdout);
+            console.log('工作树修改' + stdout);
             // 添加修改
             exec('git add *', {
                 encoding: 'utf8'
